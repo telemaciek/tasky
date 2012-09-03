fc5e59ed3b057af2cbb6a7bc110cbff98c3e0dee
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2012-09-03 20:14:43 +0200

    edited user.rb for validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, :case_sensitive => false
 end
